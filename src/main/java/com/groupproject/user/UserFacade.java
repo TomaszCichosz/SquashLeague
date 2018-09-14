d1510fbe16a5da1812b5113049c4b32903f55bbe
Tomasz Cichosz <tomaszcichosz@gmail.com>
2018-09-14 18:44:28 +0200

    Add getUserByUuid() in UserFacade

--- src/main/java/com/groupproject/user/UserFacade.java
+++ src/main/java/com/groupproject/user/UserFacade.java
@@ -1,8 +1,19 @@
 package com.groupproject.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserFacade {
 
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserFacade(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User getUserByUuid(String uuid) {
+        return userRepository.findOneByUuid(uuid);
+    }
 }
