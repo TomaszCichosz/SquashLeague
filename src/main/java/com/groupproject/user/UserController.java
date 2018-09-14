a4640a5f330824894daba465b8b61430dd76a260
MICHAŁ <mr.brozek@gmail.com>
2018-09-14 19:51:30 +0200

    Change controllers adnotations

--- src/main/java/com/groupproject/user/UserController.java
+++ src/main/java/com/groupproject/user/UserController.java
@@ -1,11 +1,12 @@
 package com.groupproject.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/api/users")
 class UserController {
 
