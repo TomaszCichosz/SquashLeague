1c0c8f0affef70ebe9b425dd1f012cb114feff12
MICHAŁ <mr.brozek@gmail.com>
2018-09-14 19:24:01 +0200

    Change MatchDto

--- src/main/java/com/groupproject/match/MatchDto.java
+++ src/main/java/com/groupproject/match/MatchDto.java
@@ -4,22 +4,22 @@ import com.groupproject.user.User;
 
 class MatchDto {
 
-    private User host;
-    private User guest;
+    private String hostUuid;
+    private String guestUuid;
 
     public MatchDto() {
     }
 
     public MatchDto(Match match) {
-        this.host = match.getHost();
-        this.guest = match.getGuest();
+        this.hostUuid = match.getHost().getUuid();
+        this.guestUuid = match.getGuest().getUuid();
     }
 
-    public User getHost() {
-        return host;
+    public String getHostUuid() {
+        return hostUuid;
     }
 
-    public User getGuest() {
-        return guest;
+    public String getGuestUuid() {
+        return guestUuid;
     }
 }
