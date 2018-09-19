b017d31dc270234d7561f5ec4339c5eb340c634e
Tomasz Cichosz <tomaszcichosz@gmail.com>
2018-09-19 20:23:29 +0200

    Modify create() in MatchServiceImpl according to new Player entity *fixed MatchDto *fixed EloRatingFacade

--- src/main/java/com/groupproject/elorating/EloRatingFacade.java
+++ src/main/java/com/groupproject/elorating/EloRatingFacade.java
@@ -1,5 +1,6 @@
 package com.groupproject.elorating;
 
+import com.groupproject.player.Player;
 import com.groupproject.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -15,11 +16,11 @@ public class EloRatingFacade {
     }
 
 
-    public int[] getNewRating(User host, User guest, String matchScore) {
+    public int[] getNewRating(Player host, Player guest, String matchScore) {
         StringBuilder sb = new StringBuilder();
 
-        return new int[]{eloRatingService.getNewRating(host.getRanking(), guest.getRanking(), matchScore),
-                eloRatingService.getNewRating(guest.getRanking(), host.getRanking(),
+        return new int[]{eloRatingService.getNewRating(host.getEloRating(), guest.getEloRating(), matchScore),
+                eloRatingService.getNewRating(guest.getEloRating(), host.getEloRating(),
                         sb.append(matchScore).reverse().toString())};
     }
 }
