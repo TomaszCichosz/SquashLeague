30cd51b617d4ceabece8a80c50d4779855d08d93
Tomasz Cichosz <tomaszcichosz@gmail.com>
2018-09-19 23:32:17 +0200

    Add id generation strategy

--- src/main/java/com/groupproject/player/Player.java
+++ src/main/java/com/groupproject/player/Player.java
@@ -12,7 +12,7 @@ import java.util.Set;
 public class Player extends BaseEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private int eloRating;
     private int gamesWon;
