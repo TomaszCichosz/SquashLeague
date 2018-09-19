1802cb2e43abcb369437b35158a37195808a6fe2
Tomasz Cichosz <tomaszcichosz@gmail.com>
2018-09-19 19:42:57 +0200

    Add fields, getters and setters to PlayerDto

--- src/main/java/com/groupproject/player/PlayerDto.java
+++ src/main/java/com/groupproject/player/PlayerDto.java
@@ -1,4 +1,35 @@
 package com.groupproject.player;
 
 class PlayerDto {
+
+    private String uuid;
+    private int eloRating;
+    private int gamesWon;
+    private int gamesLost;
+
+    public PlayerDto() {
+    }
+
+    public PlayerDto(Player player) {
+        uuid = player.getUuid();
+        eloRating = player.getEloRating();
+        gamesWon = player.getGamesWon();
+        gamesLost = player.getGamesLost();
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public int getEloRating() {
+        return eloRating;
+    }
+
+    public int getGamesWon() {
+        return gamesWon;
+    }
+
+    public int getGamesLost() {
+        return gamesLost;
+    }
 }
