fc85e8f97603ce84d4dbb893bf4b01ea388d0e56
Tomasz Cichosz <tomaszcichosz@gmail.com>
2018-09-19 20:00:10 +0200

    Refactor dtos according to updated User fields

--- src/main/java/com/groupproject/user/UserCreateDto.java
+++ src/main/java/com/groupproject/user/UserCreateDto.java
@@ -2,48 +2,15 @@ package com.groupproject.user;
 
 class UserCreateDto {
 
-    private String name;
-    private String surname;
     private String email;
     private String login;
     private String password;
-    private Integer startingRanking;
 
     public UserCreateDto() {
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
     public String getEmail() {
         return email;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public Integer getStartingRanking() {
-        return startingRanking;
-    }
-
-    public UserCreateDto setName(String name) {
-        this.name = name;
-        return this;
-    }
-
-    public UserCreateDto setSurname(String surname) {
-        this.surname = surname;
-        return this;
     }
 
     public UserCreateDto setEmail(String email) {
@@ -51,18 +18,21 @@ class UserCreateDto {
         return this;
     }
 
+    public String getLogin() {
+        return login;
+    }
+
     public UserCreateDto setLogin(String login) {
         this.login = login;
         return this;
     }
 
-    public UserCreateDto setPassword(String password) {
-        this.password = password;
-        return this;
+    public String getPassword() {
+        return password;
     }
 
-    public UserCreateDto setStartingRanking(Integer startingRanking) {
-        this.startingRanking = startingRanking;
+    public UserCreateDto setPassword(String password) {
+        this.password = password;
         return this;
     }
 }
--- src/main/java/com/groupproject/user/UserDto.java
+++ src/main/java/com/groupproject/user/UserDto.java
@@ -2,35 +2,22 @@ package com.groupproject.user;
 
 class UserDto {
 
-    private String name;
-    private String surname;
+    private String uuid;
     private String email;
     private String login;
     private String password;
-    private Integer ranking;
-    private Integer gamesWon;
-    private Integer gamesLost;
 
     public UserDto() {
     }
 
     public UserDto(User user) {
-        this.name = user.getName();
-        this.surname = user.getSurname();
         this.email = user.getEmail();
         this.login = user.getLogin();
         this.password = user.getPassword();
-        this.ranking = user.getRanking();
-        this.gamesWon = user.getGamesWon();
-        this.gamesLost = user.getGamesLost();
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getSurname() {
-        return surname;
+    public String getUuid() {
+        return uuid;
     }
 
     public String getEmail() {
@@ -43,17 +30,5 @@ class UserDto {
 
     public String getPassword() {
         return password;
-    }
-
-    public Integer getRanking() {
-        return ranking;
-    }
-
-    public Integer getGamesWon() {
-        return gamesWon;
-    }
-
-    public Integer getGamesLost() {
-        return gamesLost;
     }
 }
