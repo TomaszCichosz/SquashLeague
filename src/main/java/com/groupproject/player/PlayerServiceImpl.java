cb895d5ecae830b8ed6a0a1ba60cc8034a1e5488
Tomasz Cichosz <tomaszcichosz@gmail.com>
2018-09-19 19:19:05 +0200

    Add find methods logic in PlayerServiceImpl

--- src/main/java/com/groupproject/player/PlayerServiceImpl.java
+++ src/main/java/com/groupproject/player/PlayerServiceImpl.java
@@ -1,31 +1,40 @@
 package com.groupproject.player;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
-public class PlayerServiceImpl implements PlayerService {
+class PlayerServiceImpl implements PlayerService {
+
+    private PlayerRepository playerRepository;
+
+    @Autowired
+    public PlayerServiceImpl(PlayerRepository playerRepository) {
+        this.playerRepository = playerRepository;
+    }
 
     @Override
     public List<PlayerDto> findAll() {
-        return null;
+        return playerRepository.findAll().stream().map(PlayerDto::new).collect(Collectors.toList());
     }
 
     @Override
     public PlayerDto findOneByUuid(String uuid) {
-        return null;
+        return new PlayerDto(playerRepository.findOneByUuid(uuid));
     }
 
     @Override
-    public PlayerDto create(PlayerCreateDto playerCreateDto) {
+    public PlayerDto create(PlayerCreateDto dto) {
         return null;
     }
 
     @Override
     public void delete(String uuid) {
-
+        //TODO playerRepository.findOneByUuid(uuid).setDeleted(true);
     }
 }
