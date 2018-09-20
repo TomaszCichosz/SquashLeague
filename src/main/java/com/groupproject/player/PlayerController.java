35fd955688a0e5ffe392b844cf0efab4b3735db7
Tomasz Cichosz <tomaszcichosz@gmail.com>
2018-09-20 18:25:28 +0200

    Add getRankingView() GET Mapping in PlayerController

--- src/main/java/com/groupproject/player/PlayerController.java
+++ src/main/java/com/groupproject/player/PlayerController.java
@@ -2,6 +2,7 @@ package com.groupproject.player;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -21,5 +22,14 @@ class PlayerController {
     @GetMapping
     public List<PlayerDto> findAllPlayers() {
         return playerService.findAll();
+    }
+
+    @GetMapping("/ranking")
+    public String getRankingView(Model model) {
+        for (PlayerDto playerDto : playerService.findAll()) {
+            model.addAttribute("playerWithRating", new LoginAndRatingDto(
+                    playerDto.getUserLogin(), playerDto.getEloRating()));
+        }
+        return "ranking";
     }
 }
