533cbee311c62e9c451e7e9a7e44415849fc9342
Tomasz Cichosz <tomaszcichosz@gmail.com>
2018-09-20 17:58:20 +0200

    Add GETMapping findAllPlayers() to PlayerController

--- src/main/java/com/groupproject/player/PlayerController.java
+++ src/main/java/com/groupproject/player/PlayerController.java
@@ -2,7 +2,10 @@ package com.groupproject.player;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/api/players")
@@ -13,5 +16,10 @@ class PlayerController {
     @Autowired
     public PlayerController(PlayerService playerService) {
         this.playerService = playerService;
+    }
+
+    @GetMapping
+    public List<PlayerDto> findAllPlayers() {
+        return playerService.findAll();
     }
 }
