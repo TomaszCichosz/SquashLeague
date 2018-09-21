eba6e0a719cfd78dc397cb908131b36bd5d8111d
Tomasz Cichosz <tomaszcichosz@gmail.com>
2018-09-21 18:52:58 +0200

    Add compareTo() to LoginAndRatingDto

--- src/main/java/com/groupproject/player/LoginAndRatingDto.java
+++ src/main/java/com/groupproject/player/LoginAndRatingDto.java
@@ -1,6 +1,6 @@
 package com.groupproject.player;
 
-public class LoginAndRatingDto {
+public class LoginAndRatingDto implements Comparable<LoginAndRatingDto> {
 
     private String playerLogin;
     private int playerRating;
@@ -8,6 +8,17 @@ public class LoginAndRatingDto {
     public LoginAndRatingDto(String playerLogin, int playerRating) {
         this.playerLogin = playerLogin;
         this.playerRating = playerRating;
+    }
+
+    @Override
+    public int compareTo(LoginAndRatingDto o) {
+        if (getPlayerRating() > o.getPlayerRating()) {
+            return -1;
+        } else if (getPlayerRating() < o.getPlayerRating()) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 
     public String getPlayerLogin() {
