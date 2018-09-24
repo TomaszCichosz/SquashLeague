85d6501c0f0a75af118bdb1779347330f8c802d0
Jarek <jjankowski1@gmail.com>
2018-09-24 20:14:46 +0200

    Modified MatchController: add hostLogin get from contextHolder, * guestLogin NOT FOUND info * Wrong score info

--- src/main/java/com/groupproject/match/MatchController.java
+++ src/main/java/com/groupproject/match/MatchController.java
@@ -1,6 +1,7 @@
 package com.groupproject.match;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -27,19 +28,28 @@ class MatchController {
     @GetMapping("/add")
     public String addMatchForm(Model model) {
         MatchCreateDto dto = new MatchCreateDto();
-        //TODO get id hosta and add to dto
+        dto.setHostLogin(SecurityContextHolder.getContext().getAuthentication().getName());
+        System.out.println(dto);
         model.addAttribute("dto", dto);
         return "addmatchform";
     }
 
     @PostMapping
-    public String create(@ModelAttribute("dto") MatchCreateDto dto) {
+    public String create(@ModelAttribute("dto") MatchCreateDto dto, Model model) {
         if (!matchService.checkIfLoginExists(dto)) {
-            return "error";
+            dto.setGuestLogin("USER NOT FOUND");
+            model.addAttribute("error", "Opponent not found");
+            return "addmatchform";
         }
+        if (!true){ //TODO
+            model.addAttribute("error", "Wrong score");
+            return "addmatchform";
+        }
+//        dto.setHostLogin(SecurityContextHolder.getContext().getAuthentication().getName()); // uncomment when logged in
         System.out.println("Added to database " + dto.toString());
         matchService.create(dto);
-        return "addedmatchform";
+        model.addAttribute("added", "Match added to database");
+        return "addmatchform";
     }
 
     //TODO GET MATCH by uuid
