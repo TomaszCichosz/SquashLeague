e730be5e5f42db39f931bb0b87690d4341f6dfe1
MICHAŁ <mr.brozek@gmail.com>
2018-09-25 20:12:34 +0200

    Create failure handler view

--- src/main/java/com/groupproject/commons/IndexController.java
+++ src/main/java/com/groupproject/commons/IndexController.java
@@ -1,14 +1,20 @@
 package com.groupproject.commons;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
+    @GetMapping("/index")
     String index() {
         return "index";
     }
 
+    @GetMapping("/index/error")
+    String error(Model model){
+        model.addAttribute("msg","Wrong login or password");
+        return "index";
+    }
 }
