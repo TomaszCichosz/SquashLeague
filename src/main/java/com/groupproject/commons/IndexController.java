40e1ef746ad3586fb66fba6bdea1261004088790
MICHAŁ <mr.brozek@gmail.com>
2018-09-25 20:21:03 +0200

    Add view mapping

--- src/main/java/com/groupproject/commons/IndexController.java
+++ src/main/java/com/groupproject/commons/IndexController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
+    @GetMapping("/")
+    String view() {
+        return "index";
+    }
+
     @GetMapping("/index")
     String index() {
         return "index";
